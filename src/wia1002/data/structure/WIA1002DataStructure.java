a3a58749fae879396b07a8968902563127dc6e77
Ziqnas <haziqnasaruddin@gmail.com>
2025-03-18 12:41:18 +0800

    Initialize Repo

---
+++ src/wia1002/data/structure/WIA1002DataStructure.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wia1002.data.structure;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class WIA1002DataStructure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
