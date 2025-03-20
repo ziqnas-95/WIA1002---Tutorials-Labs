e7857fd63d148a43f35846787e4736144ba76de2
Ziqnas <haziqnasaruddin@gmail.com>
2025-03-20 14:52:45 +0800

    q4 q5 answers

---
+++ src/Tutorial1_FOP/BankAccount.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tutorial1_FOP;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+
+interface Account{
+    int deposit(int amtD);
+    boolean withdraw(int amtW);
+}
+
+
+public class BankAccount implements Account {
+    
+    public int balance;
+
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public int deposit(int amtD) {
+        this.balance += amtD;
+        return this.balance;
+    }
+
+    @Override
+    public boolean withdraw(int amtW) {
+        if (amtW <= this.balance){
+            this.balance -= amtW;
+            return true;
+        } 
+        return false;
+    }
+        
+    public static void main(String[] args) {
+        BankAccount CIMB = new BankAccount(50);
+        CIMB.deposit(100);
+        System.out.println(CIMB.withdraw(150));
+    }
+    
+}
