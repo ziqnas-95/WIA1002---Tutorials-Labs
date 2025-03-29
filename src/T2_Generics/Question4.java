c67a45cd4b5f2118129d77e7aaccaab038d9ec36
Ziqnas <haziqnasaruddin@gmail.com>
2025-03-29 21:58:11 +0800

    tutorial 2 q5 q6

---
+++ src/T2_Generics/Question4.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package T2_Generics;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class Question4 {
+    
+    public static void main(String[] args) {
+        
+        Duo<String,Integer> sideShape = new Duo("Haziq",20); 
+        System.out.println("SS: " + sideShape.getFirst());
+        System.out.println("SS: " + sideShape.getSecond());
+        
+        Duo<Double,Double> points = new Duo(25.6,26.5);
+        System.out.println("P: " + points.getFirst());
+        System.out.println("P: " + points.getSecond());
+        
+    }
+    
+}
