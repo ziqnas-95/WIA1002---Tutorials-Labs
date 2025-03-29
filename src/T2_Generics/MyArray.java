04c3c2a65350f8c924572cecc42fccfec5d289d6
Ziqnas <haziqnasaruddin@gmail.com>
2025-03-29 21:03:29 +0800

    tutorial 2 q2

---
+++ src/T2_Generics/MyArray.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package T2_Generics;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class MyArray<T> {
+    
+    public static void main(String[] args) {
+        
+        Integer[] intArray = {1,2,3,4,5};
+        String[] stringArray = {"Jane","Tom","Bob"};
+        Character[] charArray  = {'a','b','c'};
+        
+        listArray(intArray);
+        listArray(stringArray);
+        listArray(charArray);
+       
+    }
+    
+    public static <T> void listArray(T[] array){
+    
+        System.out.println("Elements in the array:");
+        for (T elements : array){
+            System.out.print(elements + " ");
+        }
+        System.out.println("\n");
+    }
+    
+    
+    
+}
