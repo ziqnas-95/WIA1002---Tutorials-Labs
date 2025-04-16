eb6dcdd93f79c88ef5db5b4dc4adbf7854e7f109
Ziqnas <haziqnasaruddin@gmail.com>
2025-04-16 23:59:01 +0800

    lab linkedlist

---
+++ src/linkedlist/MyLinkedList.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linkedlist;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class MyLinkedList<E> {
+    
+    protected static int size = 0;
+    Node<E> head, tail;
+
+    public MyLinkedList() {
+    }
+    
+    public void addFirst(E e){
+        Node<E> newNode = new Node<>(e);
+        if (size == 0){
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+    
+    public void addLast(E e){
+        Node<E> newNode = new Node<>(e);
+        if (size == 0){
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = tail.next;
+        }
+        size++;
+    }
+    
+    public void add(int index, E e){
+        Node<E> newNode = new Node<>(e);
+        if (index == 0){
+            addFirst(e);
+        } else if (index >= size){
+            addLast(e);
+        } else {
+            Node<E> current = head;
+            for (int i = 1; i < index; i++){
+                current = current.next;
+            }
+            Node<E> temp = current.next;
+            current.next = newNode;
+            (current.next).next = temp;
+            size++;
+        }
+        
+    }
+    
+    public void print(){
+        Node<E> current = head;
+        for (int i = 0; i < size; i++){
+            System.out.println(current.element);
+            current = current.next;
+        }
+        
+    
+    }
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+        MyLinkedList<String> list = new MyLinkedList<>();
+        
+        list.addFirst("h");
+        list.addFirst("ha");
+        list.addLast("haziq");
+        list.add(1, "haziq nasaruddin");
+        list.print();
+    }
+    
+}
