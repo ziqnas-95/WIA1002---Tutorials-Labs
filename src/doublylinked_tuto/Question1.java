0a475bf9a6b9029047ff848b2ab58e029f7dece2
Ziqnas <haziqnasaruddin@gmail.com>
2025-04-21 21:47:43 +0800

    doubly linked-l tutorial

---
+++ src/doublylinked_tuto/Question1.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package doublylinked_tuto;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class Question1 {
+}
+
+//    public E xyz(int index, E e) {
+//        Node<E> current = head;
+//        Node<E> temp;
+//
+//        if (index < 0) return null;
+//        else if (index >= size - 1) {
+//            this.addLast(e);
+//            return null;
+//        } else if (index == 0) {
+//            temp = head;
+//            head.element = e;
+//            return temp.element;
+//        } else {
+//            for (int i = 1; i < index; i++) {
+//                current = current.next;
+//            }
+//            temp = current.next;
+//            current.next.element = e;
+//            return temp.element;
+//        }
+//    }
+
+/**
+ *  a) Based on the above source code, explain what lines of code do from line 10-21
+ * 
+ *  
+ *  b) What is the main purpose of method xyz()?
+ * 
+ * 
+ *  c) Debug the code and make it concise and simpler.
+ */
---
+++ src/doublylinked_tuto/Question2.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package doublylinked_tuto;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class Question2 {
+}
+
+//    else {
+//        Node<E> temp = head;
+//        for (int i = 0; i < index; i++) {
+//            temp = temp.next;
+//        }
+//        element = temp.element;
+//        temp.next.prev = temp.prev;
+//        temp.prev.next = temp.next;
+//        temp.next = null;
+//        temp.prev = null;
+//        size--;
+//    }
+
+/**
+ *  a) Explain what lines of code do from line 2-11
+ * 
+ *  
+ *  b) Draw the nodes for lines 7-10.
+ * 
+ */
