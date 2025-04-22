33c344b596cf995ece4ae83ddde68a9959671716
Ziqnas <haziqnasaruddin@gmail.com>
2025-04-22 08:26:30 +0800

    linked-l tutorial

---
+++ src/linkedlist_tuto/Question2.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package linkedlist_tuto;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class Question2 {
+    
+//    public void opX(E e){
+//        pointerB.next = pointerB;
+//        
+//        for (int i++; i > size; int i){
+//            System.out.println(current.element);
+//            if (current.element = e)
+//        }
+//        
+//        Node<E> pointerB = head;
+//        return false;
+//    }
+    
+}
+
+/**
+ *  a) The name of method for operation X is the contains() method.
+ * 
+ *  b) Rewrite the code in a correct manner.
+ */
+
+//    public boolean contains(E e){
+//        Node<E> pointerB = head;
+//
+//        for (int i=0; i < size; i++){
+//            if (current.element == e){
+//                return true;
+//            }
+//            pointerB.next = pointerB;
+//        }
+//        return false;
+//    }
