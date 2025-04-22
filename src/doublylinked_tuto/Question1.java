2ac8b4a5d2791e42316d3f1e1d19d6518e8cba9e
Ziqnas <haziqnasaruddin@gmail.com>
2025-04-22 09:02:04 +0800

    linked-l tutorial

--- src/doublylinked_tuto/Question1.java
+++ src/doublylinked_tuto/Question1.java
@@ -35,10 +35,18 @@ public class Question1 {
 
 /**
  *  a) Based on the above source code, explain what lines of code do from line 10-21
+ *  -  Checks if the index is equal to 0 (which means the head node)
+ *     It assigns the current head to a temporary node and then resets the current head element to the new element.
+ *     Then it will return the previous element that has been replaced.
  * 
+ *  -  Similar to lines 10-13 but with additional steps. As it is not the head node, the lines will traverse through
+ *     the linked-list until it reaches the node before the targeted node.
+ *     Then, it will assign the next reference of the current node to a temporary node.
+ *     It will then replace next node reference's element to the new element.
+ *     In the end, the code will return the previous element that has been replaced.
  *  
  *  b) What is the main purpose of method xyz()?
- * 
+ *  -  Main purpose of the method is to replace the element in the list at a specific index
  * 
  *  c) Debug the code and make it concise and simpler.
  */
