a2a5b1e703cb7654154e345eddb086f692e093dd
Ziqnas <haziqnasaruddin@gmail.com>
2025-05-26 11:22:48 +0800

    Recursion Tutorial

--- src/recursion_lab/TutorialQuestions.java
+++ src/recursion_lab/TutorialQuestions.java
@@ -13,10 +13,37 @@ public class TutorialQuestions {
     public static void main(String[] args) {
         
         System.out.println(calculateSum(5));
-        
+        reverseString("My name is Muhammad Haziq bin Nasaruddin");
+        System.out.println(" ");
         printDigit(-4567);
         
     }
+    
+    /**
+     * QUESTION 3: Reversing a String 
+     */
+    public static void reverseString(String myStr){
+    
+        /**
+         * Base Case: When string length equals to one;
+         * Recursive Case: When string length two or more;
+         */
+        
+        if (myStr.isEmpty()){
+            System.out.print("");
+        }
+        
+        if (myStr.length() == 1){
+            System.out.print(myStr);
+        } else {
+            char tmp = myStr.charAt(myStr.length()-1);
+            System.out.print(tmp);
+            String next = myStr.substring(0, myStr.length()-1); //last index is exclusive
+            reverseString(next);
+        }
+        
+    }
+    
     
     /**
      * QUESTION 4: Calculating the sum of the integer N to 1
