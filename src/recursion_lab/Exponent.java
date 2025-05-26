d0e08d184fd8c0cdfc57e706092ad0b6ff93eb64
Ziqnas <haziqnasaruddin@gmail.com>
2025-05-26 12:01:40 +0800

    Recursion Lab Q3

---
+++ src/recursion_lab/Exponent.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package recursion_lab;
+
+/**
+ *
+ * @author Haziq Nasaruddin
+ */
+public class Exponent {
+    
+    public static void main(String[] args) {
+        
+        System.out.println(exponent(10,3));
+        
+    }
+    
+    public static long exponent(int x, int m){
+    
+        if (m > 0){
+            return x * exponent(10, m-1);
+        } else {
+            return 1;
+        }
+    }
+}
